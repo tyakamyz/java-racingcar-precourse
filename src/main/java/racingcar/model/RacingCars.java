11a02c1283a4ff29d19f8611c102779cc9a35b86
tyakamyz <tyakamyz@naver.com>
2022-04-22 22:24:50 +0900

    refactor: WinnerCars 객체 삭제
    * RacingCars에서 우승 자동차 선별되도록 로직 변경

--- src/main/java/racingcar/model/RacingCars.java
+++ src/main/java/racingcar/model/RacingCars.java
@@ -3,6 +3,8 @@ package racingcar.model;
 import java.util.List;
 
 public class RacingCars extends Cars{
+    Cars winnerCars;
+
     public RacingCars(){
         super();
     }
@@ -14,6 +16,20 @@ public class RacingCars extends Cars{
     public void registerCars(List<String> carNames){
         for (String name : carNames) {
             addCar(new Car(name));
+        }
+    }
+
+    public void findWinnerCars() {
+        this.winnerCars = new Cars();
+
+        for(Car car : this.cars){
+            compareMaxScore(car);
+        }
+    }
+
+    private void compareMaxScore(Car car) {
+        if(car.isWinner()){
+            winnerCars.addCar(car);
         }
     }
 }
